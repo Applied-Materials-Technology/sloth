8df419331014e6b9bac88a248d3cd2b12c7a19b5
Rory Spencer <rory.spencer@ukaea.uk>
2025-03-21 12:40:29 +0000

    PerzynaViscoplasticityStressUpdate is now AD compatible

--- include/materials/PerzynaViscoplasticityStressUpdateFunction.h
+++ include/materials/PerzynaViscoplasticityStressUpdateFunction.h
@@ -15,38 +15,49 @@
  * This class is based on the implicit integration algorithm in F. Dunne and N.
  * Petrinic's Introduction to Computational Plasticity (2004) Oxford University
  * Press, pg. 162 - 163.
- * 
+ *
  * Expanded to permit function-based strain hardening rather than linear hardening.
  * Basing chages on IsotropicPlasticityStressUpdate approach, which also derives
  * from Dunne & Petrinic.
  */
-class PerzynaViscoplasticityStressUpdateFunction : public RadialReturnStressUpdate
+template <bool is_ad>
+class PerzynaViscoplasticityStressUpdateFunctionTempl : public RadialReturnStressUpdateTempl<is_ad>
 {
 public:
   static InputParameters validParams();
 
-  PerzynaViscoplasticityStressUpdateFunction(const InputParameters & parameters);
+  PerzynaViscoplasticityStressUpdateFunctionTempl(const InputParameters & parameters);
+
+  using Material::_qp;
+  using RadialReturnStressUpdateTempl<is_ad>::_base_name;
+  using RadialReturnStressUpdateTempl<is_ad>::_three_shear_modulus;
+  using RadialReturnStressUpdateTempl<is_ad>::_dt;
 
 protected:
   virtual void initQpStatefulProperties() override;
   virtual void propagateQpStatefulProperties() override;
 
-  virtual void computeStressInitialize(const Real & effective_trial_stress,
-                                       const RankFourTensor & elasticity_tensor) override;
-  virtual Real computeResidual(const Real & effective_trial_stress, const Real & scalar) override;
-  virtual Real computeDerivative(const Real & effective_trial_stress, const Real & scalar) override;
-  virtual void iterationFinalize(const Real & scalar) override;
-  virtual void computeStressFinalize(const RankTwoTensor & plasticStrainIncrement) override;
-  virtual Real computeHardeningValue(Real scalar);
-  virtual Real computeHardeningDerivative(Real scalar); // Added. 
+  virtual void
+  computeStressInitialize(const GenericReal<is_ad> & effective_trial_stress,
+                          const GenericRankFourTensor<is_ad> & elasticity_tensor) override;
+  virtual GenericReal<is_ad> computeResidual(const GenericReal<is_ad> & effective_trial_stress,
+                                             const GenericReal<is_ad> & scalar) override;
+  virtual GenericReal<is_ad> computeDerivative(const GenericReal<is_ad> & effective_trial_stress,
+                                               const GenericReal<is_ad> & scalar) override;
+  virtual void iterationFinalize(const GenericReal<is_ad> & scalar) override;
+  virtual void
+  computeStressFinalize(const GenericRankTwoTensor<is_ad> & plasticStrainIncrement) override;
+  virtual GenericReal<is_ad> computeHardeningValue(const GenericReal<is_ad> & scalar);
+  virtual GenericReal<is_ad> computeHardeningDerivative(const GenericReal<is_ad> & scalar);
 
   /// a string to prepend to the plastic strain Material Property name
   const std::string _plastic_prepend;
 
   ///@{ Strain hardening parameters
-  const Real _yield_stress; //Constant for now, but in IsotropicPlasticityStressUpdate it can be a function of temperature.
-  const Function & _hardening_function; // Modified to function.
-  Real _hardening_slope; // Added, stores current gradient of function
+  const Real _yield_stress; // Constant for now, but in IsotropicPlasticityStressUpdate it can be a
+  // function of temperature.
+  const Function * const _hardening_function; // Modified to function.
+  GenericReal<is_ad> _hardening_slope;        // Added, stores current gradient of function
   ///@}
 
   ///@{ Viscoplasticity constitutive equation parameters
@@ -55,19 +66,26 @@ protected:
   ///@}
 
   /// Elastic properties
-  Real _yield_condition;
+  GenericReal<is_ad> _yield_condition;
 
   ///@{ Viscoplasticity terms corresponding to Dunne and Petrinic eqn 5.64
-  Real _xphir;
-  Real _xphidp;
+  GenericReal<is_ad> _xphir;
+  GenericReal<is_ad> _xphidp;
   ///@}
 
-  MaterialProperty<Real> & _hardening_variable;
+  GenericMaterialProperty<Real, is_ad> & _hardening_variable;
   const MaterialProperty<Real> & _hardening_variable_old;
 
   /// plastic strain of this model
-  MaterialProperty<RankTwoTensor> & _plastic_strain;
+  GenericMaterialProperty<RankTwoTensor, is_ad> & _plastic_strain;
 
   /// old value of plastic strain
   const MaterialProperty<RankTwoTensor> & _plastic_strain_old;
+
+  std::vector<GenericMaterialProperty<Real, is_ad> *> _properties;
 };
+
+typedef PerzynaViscoplasticityStressUpdateFunctionTempl<false>
+    PerzynaViscoplasticityStressUpdateFunction;
+typedef PerzynaViscoplasticityStressUpdateFunctionTempl<true>
+    ADPerzynaViscoplasticityStressUpdateFunction;
--- src/materials/PerzynaViscoplasticituStressUpdateFunction.C
+++ src/materials/PerzynaViscoplasticituStressUpdateFunction.C
@@ -4,11 +4,13 @@
 #include "ElasticityTensorTools.h"
 
 registerMooseObject("SolidMechanicsApp", PerzynaViscoplasticityStressUpdateFunction);
+registerMooseObject("SolidMechanicsApp", ADPerzynaViscoplasticityStressUpdateFunction);
 
+template <bool is_ad>
 InputParameters
-PerzynaViscoplasticityStressUpdateFunction::validParams()
+PerzynaViscoplasticityStressUpdateFunctionTempl<is_ad>::validParams()
 {
-  InputParameters params = RadialReturnStressUpdate::validParams();
+  InputParameters params = RadialReturnStressUpdateTempl<is_ad>::validParams();
   params.addClassDescription("This class uses the discrete material for a Perzyna type "
                              "viscoplasticity model in which the effective plastic strain is "
                              "solved for using a creep approach.");
@@ -16,13 +18,12 @@ PerzynaViscoplasticityStressUpdateFunction::validParams()
   // Non-linear function strain hardening parameters
   params.addRequiredParam<Real>("yield_stress",
                                 "The point at which plastic strain begins accumulating");
-  params.addRequiredParam<FunctionName>("hardening_function","True Stress as a function of plastic stain");
+  params.addRequiredParam<FunctionName>("hardening_function",
+                                        "True Stress as a function of plastic stain");
 
   // Viscoplasticity constitutive equation parameters
-  params.addRequiredParam<Real>("n",
-                                "Viscoplasticity coefficient, power law exponent");
-  params.addRequiredParam<Real>("eta",
-                                "Viscoplasticity coefficient viscosity / drag stress");
+  params.addRequiredParam<Real>("n", "Viscoplasticity coefficient, power law exponent");
+  params.addRequiredParam<Real>("eta", "Viscoplasticity coefficient viscosity / drag stress");
   params.addDeprecatedParam<std::string>(
       "plastic_prepend",
       "",
@@ -33,47 +34,56 @@ PerzynaViscoplasticityStressUpdateFunction::validParams()
   return params;
 }
 
-PerzynaViscoplasticityStressUpdateFunction::PerzynaViscoplasticityStressUpdateFunction(
-    const InputParameters & parameters)
-  : RadialReturnStressUpdate(parameters),
-    _plastic_prepend(getParam<std::string>("plastic_prepend")),
-    _yield_stress(parameters.get<Real>("yield_stress")),
-    _hardening_function(getFunction("hardening_function")), // Added but no clue if this will work as other class is based on templates.
+template <bool is_ad>
+PerzynaViscoplasticityStressUpdateFunctionTempl<
+    is_ad>::PerzynaViscoplasticityStressUpdateFunctionTempl(const InputParameters & parameters)
+  : RadialReturnStressUpdateTempl<is_ad>(parameters),
+    _plastic_prepend(this->template getParam<std::string>("plastic_prepend")),
+    _yield_stress(this->isParamValid("yield_stress") ? this->template getParam<Real>("yield_stress")
+                                                     : 0),
+    _hardening_function(this->isParamValid("hardening_function")
+                            ? &this->getFunction("hardening_function")
+                            : nullptr),
     _hardening_slope(0.0),
     _n(parameters.get<Real>("n")),
     _eta(parameters.get<Real>("eta")),
     _yield_condition(-1.0), // set to a non-physical value to catch uninitalized yield condition
-    _hardening_variable(declareProperty<Real>("hardening_variable")),
-    _hardening_variable_old(getMaterialPropertyOld<Real>("hardening_variable")),
+    _hardening_variable(this->template declareGenericProperty<Real, is_ad>("hardening_variable")),
+    _hardening_variable_old(this->template getMaterialPropertyOld<Real>("hardening_variable")),
 
-    _plastic_strain(
-        declareProperty<RankTwoTensor>(_base_name + _plastic_prepend + "plastic_strain")),
-    _plastic_strain_old(
-        getMaterialPropertyOld<RankTwoTensor>(_base_name + _plastic_prepend + "plastic_strain"))
+    _plastic_strain(this->template declareGenericProperty<RankTwoTensor, is_ad>(
+        _base_name + _plastic_prepend + "plastic_strain")),
+    _plastic_strain_old(this->template getMaterialPropertyOld<RankTwoTensor>(
+        _base_name + _plastic_prepend + "plastic_strain"))
 {
 }
 
+template <bool is_ad>
 void
-PerzynaViscoplasticityStressUpdateFunction::initQpStatefulProperties()
+PerzynaViscoplasticityStressUpdateFunctionTempl<is_ad>::initQpStatefulProperties()
 {
   _hardening_variable[_qp] = 0.0;
   _plastic_strain[_qp].zero();
 }
 
+template <bool is_ad>
 void
-PerzynaViscoplasticityStressUpdateFunction::propagateQpStatefulProperties()
+PerzynaViscoplasticityStressUpdateFunctionTempl<is_ad>::propagateQpStatefulProperties()
 {
   _hardening_variable[_qp] = _hardening_variable_old[_qp];
   _plastic_strain[_qp] = _plastic_strain_old[_qp];
 
-  propagateQpStatefulPropertiesRadialReturn();
+  RadialReturnStressUpdateTempl<is_ad>::propagateQpStatefulPropertiesRadialReturn();
 }
 
+template <bool is_ad>
 void
-PerzynaViscoplasticityStressUpdateFunction::computeStressInitialize(
-    const Real & effective_trial_stress, const RankFourTensor & elasticity_tensor)
+PerzynaViscoplasticityStressUpdateFunctionTempl<is_ad>::computeStressInitialize(
+    const GenericReal<is_ad> & effective_trial_stress,
+    const GenericRankFourTensor<is_ad> & elasticity_tensor)
 {
-  RadialReturnStressUpdate::computeStressInitialize(effective_trial_stress, elasticity_tensor);
+  RadialReturnStressUpdateTempl<is_ad>::computeStressInitialize(effective_trial_stress,
+                                                                elasticity_tensor);
 
   _yield_condition = effective_trial_stress - _hardening_variable_old[_qp] - _yield_stress;
 
@@ -81,11 +91,12 @@ PerzynaViscoplasticityStressUpdateFunction::computeStressInitialize(
   _plastic_strain[_qp] = _plastic_strain_old[_qp];
 }
 
-Real
-PerzynaViscoplasticityStressUpdateFunction::computeResidual(const Real & effective_trial_stress,
-                                                       const Real & scalar)
+template <bool is_ad>
+GenericReal<is_ad>
+PerzynaViscoplasticityStressUpdateFunctionTempl<is_ad>::computeResidual(
+    const GenericReal<is_ad> & effective_trial_stress, const GenericReal<is_ad> & scalar)
 {
-  Real residual = 0.0;
+  GenericReal<is_ad> residual = 0.0;
 
   mooseAssert(_yield_condition != -1.0,
               "the yield stress was not updated by computeStressInitialize");
@@ -94,63 +105,81 @@ PerzynaViscoplasticityStressUpdateFunction::computeResidual(const Real & effecti
   {
     _hardening_slope = computeHardeningDerivative(scalar);
 
-    const Real xflow =  ((effective_trial_stress - (_three_shear_modulus * scalar))/
-                                  (computeHardeningValue(scalar) + _yield_stress)) - 1;
-    const Real xphi = _eta*std::pow(xflow,_n);
+    const GenericReal<is_ad> xflow = ((effective_trial_stress - (_three_shear_modulus * scalar)) /
+                                      (computeHardeningValue(scalar) + _yield_stress)) -
+                                     1;
+    const GenericReal<is_ad> xphi = _eta * std::pow(xflow, _n);
 
-    _xphidp = ((-_three_shear_modulus * _eta * _n)/(computeHardeningValue(scalar) + _yield_stress))*std::pow(xflow,_n-1);
-    _xphir = -_eta * _n*((effective_trial_stress - (_three_shear_modulus * scalar))/std::pow((computeHardeningValue(scalar) + _yield_stress),2)) * std::pow(xflow,_n-1);
+    _xphidp =
+        ((-_three_shear_modulus * _eta * _n) / (computeHardeningValue(scalar) + _yield_stress)) *
+        std::pow(xflow, _n - 1);
+    _xphir = -_eta * _n *
+             ((effective_trial_stress - (_three_shear_modulus * scalar)) /
+              std::pow((computeHardeningValue(scalar) + _yield_stress), 2)) *
+             std::pow(xflow, _n - 1);
     residual = xphi * _dt - scalar;
   }
   return residual;
 }
 
-Real
-PerzynaViscoplasticityStressUpdateFunction::computeDerivative(const Real & /*effective_trial_stress*/,
-                                                         const Real & /*scalar*/)
+template <bool is_ad>
+GenericReal<is_ad>
+PerzynaViscoplasticityStressUpdateFunctionTempl<is_ad>::computeDerivative(
+    const GenericReal<is_ad> & /*effective_trial_stress*/, const GenericReal<is_ad> & /*scalar*/)
 {
-  Real derivative = 1.0;
+  GenericReal<is_ad> derivative = 1.0;
   if (_yield_condition > 0.0)
     derivative = _xphidp * _dt + _hardening_slope * _xphir * _dt - 1.0;
 
   return derivative;
 }
 
+template <bool is_ad>
 void
-PerzynaViscoplasticityStressUpdateFunction::iterationFinalize(const Real & scalar)
+PerzynaViscoplasticityStressUpdateFunctionTempl<is_ad>::iterationFinalize(
+    const GenericReal<is_ad> & scalar)
 {
   if (_yield_condition > 0.0)
     _hardening_variable[_qp] = computeHardeningValue(scalar);
-    //std::cout << _xphidp;
-    //std::cout << ';';
-    //std::cout << _xphir;
-    //std::cout << '\n';
+  // std::cout << _xphidp;
+  // std::cout << ';';
+  // std::cout << _xphir;
+  // std::cout << '\n';
 
-    //std::cout << scalar;
-    //std::cout << ';';
-    //std::cout << computeHardeningDerivative(scalar);
-    //std::cout << ';';
-    //std::cout << computeHardeningValue(scalar);
-    //std::cout << '\n';
-
+  // std::cout << scalar;
+  // std::cout << ';';
+  // std::cout << computeHardeningDerivative(scalar);
+  // std::cout << ';';
+  // std::cout << computeHardeningValue(scalar);
+  // std::cout << '\n';
 }
 
-Real
-PerzynaViscoplasticityStressUpdateFunction::computeHardeningValue(Real scalar)
+template <bool is_ad>
+GenericReal<is_ad>
+PerzynaViscoplasticityStressUpdateFunctionTempl<is_ad>::computeHardeningValue(
+    const GenericReal<is_ad> & scalar)
 {
-  //return _hardening_variable_old[_qp] + (_hardening_slope * scalar);
-  return _hardening_function.value(_effective_inelastic_strain_old[_qp] + scalar);// - _yield_stress; // Should be the main change gets the hardening value using the function 
+  // return _hardening_variable_old[_qp] + (_hardening_slope * scalar);
+  return _hardening_function->value(this->_effective_inelastic_strain_old[_qp] +
+                                    scalar); // - _yield_stress; // Should be the main change gets
+                                             // the hardening value using the function
 }
 
-Real
-PerzynaViscoplasticityStressUpdateFunction::computeHardeningDerivative(Real scalar)
+template <bool is_ad>
+GenericReal<is_ad>
+PerzynaViscoplasticityStressUpdateFunctionTempl<is_ad>::computeHardeningDerivative(
+    const GenericReal<is_ad> & scalar)
 {
-    return _hardening_function.timeDerivative(_effective_inelastic_strain_old[_qp]);
+  return _hardening_function->timeDerivative(this->_effective_inelastic_strain_old[_qp]);
 }
 
+template <bool is_ad>
 void
-PerzynaViscoplasticityStressUpdateFunction::computeStressFinalize(
-    const RankTwoTensor & plasticStrainIncrement)
+PerzynaViscoplasticityStressUpdateFunctionTempl<is_ad>::computeStressFinalize(
+    const GenericRankTwoTensor<is_ad> & plasticStrainIncrement)
 {
   _plastic_strain[_qp] += plasticStrainIncrement;
 }
+
+template class PerzynaViscoplasticityStressUpdateFunctionTempl<false>;
+template class PerzynaViscoplasticityStressUpdateFunctionTempl<true>;
