5f0e3bad47e76bf3c6b6cb7db2ce2766ce4d047d
Rory Spencer <rory.spencer@ukaea.uk>
2025-03-24 08:29:38 +0000

    Ad compatible Sinh Viscoplastic model

---
+++ include/materials/SinhViscoplasticityStressUpdate.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "RadialReturnStressUpdate.h"
+
+/**
+ * This class uses the Discrete material in an isotropic radial return hyperbolic
+ * sine viscoplasticity model.
+ *
+ * This class inherits from RadialReturnStressUpdate and must be used
+ * in conjunction with ComputeReturnMappingStress. This uniaxial viscoplasticity
+ * class computes the plastic strain as a stateful material property.  The
+ * constitutive equation for scalar plastic strain rate used in this model is
+ * /f$ \dot{p} = \phi (\sigma_e , r) = \alpha sinh \beta (\sigma_e -r - \sigma_y) f/$
+ *
+ * This class is based on the implicit integration algorithm in F. Dunne and N.
+ * Petrinic's Introduction to Computational Plasticity (2004) Oxford University
+ * Press, pg. 162 - 163.
+ *
+ * Expanded to permit function-based strain hardening rather than linear hardening.
+ * Basing chages on IsotropicPlasticityStressUpdate approach, which also derives
+ * from Dunne & Petrinic.
+ */
+template <bool is_ad>
+class SinhViscoplasticityStressUpdateTempl : public RadialReturnStressUpdateTempl<is_ad>
+{
+public:
+  static InputParameters validParams();
+
+  SinhViscoplasticityStressUpdateTempl(const InputParameters & parameters);
+
+  using Material::_qp;
+  using RadialReturnStressUpdateTempl<is_ad>::_base_name;
+  using RadialReturnStressUpdateTempl<is_ad>::_three_shear_modulus;
+  using RadialReturnStressUpdateTempl<is_ad>::_dt;
+
+protected:
+  virtual void initQpStatefulProperties() override;
+  virtual void propagateQpStatefulProperties() override;
+
+  virtual void
+  computeStressInitialize(const GenericReal<is_ad> & effective_trial_stress,
+                          const GenericRankFourTensor<is_ad> & elasticity_tensor) override;
+  virtual GenericReal<is_ad> computeResidual(const GenericReal<is_ad> & effective_trial_stress,
+                                             const GenericReal<is_ad> & scalar) override;
+  virtual GenericReal<is_ad> computeDerivative(const GenericReal<is_ad> & effective_trial_stress,
+                                               const GenericReal<is_ad> & scalar) override;
+  virtual void iterationFinalize(const GenericReal<is_ad> & scalar) override;
+  virtual void
+  computeStressFinalize(const GenericRankTwoTensor<is_ad> & plasticStrainIncrement) override;
+  virtual GenericReal<is_ad> computeHardeningValue(const GenericReal<is_ad> & scalar);
+  virtual GenericReal<is_ad> computeHardeningDerivative(const GenericReal<is_ad> & scalar);
+
+  /// a string to prepend to the plastic strain Material Property name
+  const std::string _plastic_prepend;
+
+  ///@{ Strain hardening parameters
+  const Real _yield_stress; // Constant for now, but in IsotropicPlasticityStressUpdate it can be a
+  // function of temperature.
+  const Function * const _hardening_function; // Modified to function.
+  GenericReal<is_ad> _hardening_slope;        // Added, stores current gradient of function
+  ///@}
+
+  ///@{ Viscoplasticity constitutive equation parameters
+  const Real _alpha;
+  const Real _beta;
+  ///@}
+
+  /// Elastic properties
+  GenericReal<is_ad> _yield_condition;
+
+  ///@{ Viscoplasticity terms corresponding to Dunne and Petrinic eqn 5.64
+  GenericReal<is_ad> _xphir;
+  GenericReal<is_ad> _xphidp;
+  ///@}
+
+  GenericMaterialProperty<Real, is_ad> & _hardening_variable;
+  const MaterialProperty<Real> & _hardening_variable_old;
+
+  /// plastic strain of this model
+  GenericMaterialProperty<RankTwoTensor, is_ad> & _plastic_strain;
+
+  /// old value of plastic strain
+  const MaterialProperty<RankTwoTensor> & _plastic_strain_old;
+
+  std::vector<GenericMaterialProperty<Real, is_ad> *> _properties;
+};
+
+typedef SinhViscoplasticityStressUpdateTempl<false>
+    SinhViscoplasticityStressUpdate;
+typedef SinhViscoplasticityStressUpdateTempl<true>
+    ADSinhViscoplasticityStressUpdate;
---
+++ src/materials/SinhViscoplasticityStressUpdate.C
@@ -0,0 +1,172 @@
+#include "SinhViscoplasticityStressUpdate.h"
+
+#include "Function.h"
+#include "ElasticityTensorTools.h"
+
+registerMooseObject("SolidMechanicsApp", SinhViscoplasticityStressUpdate);
+registerMooseObject("SolidMechanicsApp", ADSinhViscoplasticityStressUpdate);
+
+template <bool is_ad>
+InputParameters
+SinhViscoplasticityStressUpdateTempl<is_ad>::validParams()
+{
+  InputParameters params = RadialReturnStressUpdateTempl<is_ad>::validParams();
+  params.addClassDescription("This class uses the discrete material for a Perzyna type "
+                             "viscoplasticity model in which the effective plastic strain is "
+                             "solved for using a creep approach.");
+
+  // Non-linear function strain hardening parameters
+  params.addRequiredParam<Real>("yield_stress",
+                                "The point at which plastic strain begins accumulating");
+  params.addRequiredParam<FunctionName>("hardening_function",
+                                        "True Stress as a function of plastic stain");
+
+  // Viscoplasticity constitutive equation parameters
+  params.addRequiredParam<Real>("alpha",
+                                "Viscoplasticity coefficient, scales the hyperbolic function");
+  params.addRequiredParam<Real>("beta",
+                                "Viscoplasticity coefficient inside the hyperbolic sin function");
+  params.addDeprecatedParam<std::string>(
+      "plastic_prepend",
+      "",
+      "String that is prepended to the plastic_strain Material Property",
+      "This has been replaced by the 'base_name' parameter");
+  params.set<std::string>("effective_inelastic_strain_name") = "effective_plastic_strain";
+
+  return params;
+}
+
+template <bool is_ad>
+SinhViscoplasticityStressUpdateTempl<is_ad>::SinhViscoplasticityStressUpdateTempl(
+    const InputParameters & parameters)
+  : RadialReturnStressUpdateTempl<is_ad>(parameters),
+    _plastic_prepend(this->template getParam<std::string>("plastic_prepend")),
+    _yield_stress(this->isParamValid("yield_stress") ? this->template getParam<Real>("yield_stress")
+                                                     : 0),
+    _hardening_function(this->isParamValid("hardening_function")
+                            ? &this->getFunction("hardening_function")
+                            : nullptr),
+    _hardening_slope(0.0),
+    _alpha(parameters.get<Real>("alpha")),
+    _beta(parameters.get<Real>("beta")),
+    _yield_condition(-1.0), // set to a non-physical value to catch uninitalized yield condition
+    _hardening_variable(this->template declareGenericProperty<Real, is_ad>("hardening_variable")),
+    _hardening_variable_old(this->template getMaterialPropertyOld<Real>("hardening_variable")),
+
+    _plastic_strain(this->template declareGenericProperty<RankTwoTensor, is_ad>(
+        _base_name + _plastic_prepend + "plastic_strain")),
+    _plastic_strain_old(this->template getMaterialPropertyOld<RankTwoTensor>(
+        _base_name + _plastic_prepend + "plastic_strain"))
+{
+}
+
+template <bool is_ad>
+void
+SinhViscoplasticityStressUpdateTempl<is_ad>::initQpStatefulProperties()
+{
+  _hardening_variable[_qp] = 0.0;
+  _plastic_strain[_qp].zero();
+}
+
+template <bool is_ad>
+void
+SinhViscoplasticityStressUpdateTempl<is_ad>::propagateQpStatefulProperties()
+{
+  _hardening_variable[_qp] = _hardening_variable_old[_qp];
+  _plastic_strain[_qp] = _plastic_strain_old[_qp];
+
+  RadialReturnStressUpdateTempl<is_ad>::propagateQpStatefulPropertiesRadialReturn();
+}
+
+template <bool is_ad>
+void
+SinhViscoplasticityStressUpdateTempl<is_ad>::computeStressInitialize(
+    const GenericReal<is_ad> & effective_trial_stress,
+    const GenericRankFourTensor<is_ad> & elasticity_tensor)
+{
+  RadialReturnStressUpdateTempl<is_ad>::computeStressInitialize(effective_trial_stress,
+                                                                elasticity_tensor);
+
+  _yield_condition = effective_trial_stress - _hardening_variable_old[_qp] - _yield_stress;
+
+  _hardening_variable[_qp] = _hardening_variable_old[_qp];
+  _plastic_strain[_qp] = _plastic_strain_old[_qp];
+}
+
+template <bool is_ad>
+GenericReal<is_ad>
+SinhViscoplasticityStressUpdateTempl<is_ad>::computeResidual(
+    const GenericReal<is_ad> & effective_trial_stress, const GenericReal<is_ad> & scalar)
+{
+  GenericReal<is_ad> residual = 0.0;
+
+  mooseAssert(_yield_condition != -1.0,
+              "the yield stress was not updated by computeStressInitialize");
+
+  if (_yield_condition > 0.0)
+  {
+    _hardening_slope = computeHardeningDerivative(scalar);
+
+    const GenericReal<is_ad> xflow =
+        _beta * (effective_trial_stress - (_three_shear_modulus * scalar) -
+                 computeHardeningValue(scalar) - _yield_stress);
+    const GenericReal<is_ad> xphi = _alpha * std::sinh(xflow);
+
+    _xphidp = -_three_shear_modulus * _alpha * _beta * std::cosh(xflow);
+    _xphir = -_alpha * _beta * std::cosh(xflow);
+    residual = xphi * _dt - scalar;
+    residual = xphi * _dt - scalar;
+  }
+  return residual;
+}
+
+template <bool is_ad>
+GenericReal<is_ad>
+SinhViscoplasticityStressUpdateTempl<is_ad>::computeDerivative(
+    const GenericReal<is_ad> & /*effective_trial_stress*/, const GenericReal<is_ad> & /*scalar*/)
+{
+  GenericReal<is_ad> derivative = 1.0;
+  if (_yield_condition > 0.0)
+    derivative = _xphidp * _dt + _hardening_slope * _xphir * _dt - 1.0;
+
+  return derivative;
+}
+
+template <bool is_ad>
+void
+SinhViscoplasticityStressUpdateTempl<is_ad>::iterationFinalize(const GenericReal<is_ad> & scalar)
+{
+  if (_yield_condition > 0.0)
+    _hardening_variable[_qp] = computeHardeningValue(scalar);
+
+}
+
+template <bool is_ad>
+GenericReal<is_ad>
+SinhViscoplasticityStressUpdateTempl<is_ad>::computeHardeningValue(
+    const GenericReal<is_ad> & scalar)
+{
+  // return _hardening_variable_old[_qp] + (_hardening_slope * scalar);
+  return _hardening_function->value(this->_effective_inelastic_strain_old[_qp] +
+                                    scalar); // - _yield_stress; // Should be the main change gets
+                                             // the hardening value using the function
+}
+
+template <bool is_ad>
+GenericReal<is_ad>
+SinhViscoplasticityStressUpdateTempl<is_ad>::computeHardeningDerivative(
+    const GenericReal<is_ad> & scalar)
+{
+  return _hardening_function->timeDerivative(this->_effective_inelastic_strain_old[_qp]);
+}
+
+template <bool is_ad>
+void
+SinhViscoplasticityStressUpdateTempl<is_ad>::computeStressFinalize(
+    const GenericRankTwoTensor<is_ad> & plasticStrainIncrement)
+{
+  _plastic_strain[_qp] += plasticStrainIncrement;
+}
+
+template class SinhViscoplasticityStressUpdateTempl<false>;
+template class SinhViscoplasticityStressUpdateTempl<true>;
